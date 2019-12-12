be44a41a5ffb50a163ea55e207a29b6c204915f2
pallavidogra <pallavi.dogra@hotmail.com>
2019-12-12 22:09:26 +0530

    addded all fields to the modal

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -11,10 +11,26 @@ $( document ).ready(function() {
 			}, 300);
 		});
 	});
+    
     $(function() {
-        $('.pop').on('click', function() {
-        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');   
-    });   
-  });
+    	$('.pop').on('click', function() {
+    		var image_id = $(this).find('img').attr('id');
+			var post_title = "#post-title-" + image_id;
+			var post_description = "#post-description-" + image_id;
+			var post_date_posted = "#post-date-posted-" + image_id;
+			var post_author = "#post-author-" + image_id;
+			var post_text_title = $(post_title).text();
+			var post_text_description = $(post_description).text();
+			var post_text_date_posted = $(post_date_posted).text();
+			var post_text_author = $(post_author).text();
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('.post-title-p').text(post_text_title)
+			$('.post-description-p').text(post_text_description)
+			$('.post-date-posted-p').text(post_text_date_posted)
+			$('.post-author-p').text(post_text_author)
+			$('#imagemodal').modal('show');
+		})
+    }); 
+
 });
+
