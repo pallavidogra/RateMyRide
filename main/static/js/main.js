b9c4c128e452e1dafa58be274d5d79d2a112d8db
pallavidogra <pallavi.dogra@hotmail.com>
2019-12-18 19:40:02 +0530

    added functionality to move to next or previous image through arrows

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,43 +1,49 @@
 $( document ).ready(function() {
-	$(function() {
-		var selectedClass = "";
-		$(".filter").click(function(){
-			selectedClass = $(this).attr("data-rel");
-			$("#gallery").fadeTo(100, 0.1);
-			$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-			setTimeout(function() {
-			$("."+selectedClass).fadeIn().addClass('animation');
-			$("#gallery").fadeTo(300, 1);
-			}, 300);
-		});
+	$('.pop').on('click', function(e) {
+		var image_id = $(this).find('img').attr('id');
+		var post_title = "#post-title-" + image_id;
+		var post_description = "#post-description-" + image_id;
+		var post_date_posted = "#post-date-posted-" + image_id;
+		var post_author = "#post-author-" + image_id;
+		var post_text_title = $(post_title).text();
+		var post_text_description = $(post_description).text();
+		var post_text_date_posted = $(post_date_posted).text();
+		var post_text_author = $(post_author).text();
+		$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+		$('.post-title-p').text(post_text_title)
+		$('.post-description-p').text(post_text_description)
+		$('.post-date-posted-p').text(post_text_date_posted)
+		$('.post-author-p').text(post_text_author)
+		$('#imagemodal').modal('show');
+		currentProfile = $(this);
+
 	});
+	$(".next").click(function(event){
+      var posts = currentProfile.parents('.gallery').find(".pop");
+      console.log('posts',posts)
+      var currentPostIndex = posts.index(currentProfile);
+      if(posts.length > (currentPostIndex + 1)) {
+        posts.get(currentPostIndex + 1).click();
+        setTimeout(function() {
+          $("#imagemodal").modal("show");
+        },500);
+      } else {
+        alert("You are on the last card!");
+      }
+    });
     
-    $(function() {
-    	$('.pop').on('click', function() {
-    		var image_id = $(this).find('img').attr('id');
-			var post_title = "#post-title-" + image_id;
-			var post_description = "#post-description-" + image_id;
-			var post_date_posted = "#post-date-posted-" + image_id;
-			var post_author = "#post-author-" + image_id;
-			var post_text_title = $(post_title).text();
-			var post_text_description = $(post_description).text();
-			var post_text_date_posted = $(post_date_posted).text();
-			var post_text_author = $(post_author).text();
-			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-			$('.post-title-p').text(post_text_title)
-			$('.post-description-p').text(post_text_description)
-			$('.post-date-posted-p').text(post_text_date_posted)
-			$('.post-author-p').text(post_text_author)
-			$('#imagemodal').modal('show');
-			$('.icon').addClass('glyphicon glyphicon-chevron-right');
-			$('.icon').css('margin-top','55px');
-			$('.icon').css('float','right');
-			$('.icon').css('font-size','30px');
-			$('.icon-left').addClass('glyphicon glyphicon-chevron-left');
-			$('.icon-left').css('margin-top','215px');
-			$('.icon-left').css('font-size','30px');			
-		})
-    }); 
-
-});
-
+    $(".prev").click(function(event){
+      var posts = currentProfile.parents('.gallery').find(".pop");
+      console.log('posts',posts)
+      var currentPostIndex = posts.index(currentProfile);
+      if(currentPostIndex > 0) {
+        posts.get(currentPostIndex - 1).click();
+        setTimeout(function() {
+          $("#imagemodal").modal("show");
+        },500);
+      } else {
+        alert("You are on the first card!");
+      }
+      
+    });
+}); 
