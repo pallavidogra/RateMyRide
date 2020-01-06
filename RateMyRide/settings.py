fb75641cc5141d9842604cab41428ae578c07d3b
pallavidogra <pallavi.dogra@hotmail.com>
2020-01-06 19:44:19 +0530

    configured static files

--- RateMyRide/settings.py
+++ RateMyRide/settings.py
@@ -97,6 +97,12 @@ DATABASES = {
     }
 }
 
+
+
+
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -134,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
