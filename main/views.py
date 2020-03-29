b729bda08577039fb9b6da3041b48d49e9809bdb
gurjeetameotech <gurjeetameotech79@gmail.com>
2020-03-29 15:09:09 +0530

    added more robust view to handle the comments section of the post

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
@@ -131,6 +131,35 @@ def add_comment(request):
         review_obj.save()
     return JsonResponse({'error': False})
 
+def post_detail(request):
+    template_name = "post_detail.html"
+    post = get_object_or_404(Post)
+    comments = post.comments.filter(active=True).order_by("-created_on")
+    new_comment = None
+    # Comment posted
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        template_name,
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
 
 def get_rating(request):
     post_id  = request.POST.get("post_id")
